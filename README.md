eaa86795b01298f854570d0560eb95979c6f6884
Piotr Kołodyński <hehlolrofl@gmail.com>
2020-02-27 19:26:25 +0100

    zadanko domowe

---
+++ README.md
@@ -0,0 +1,6 @@
+1. `npm init --yes`
+1. `npm i express`
+1. 'stworzyć 
+1. Iphone(Piotr) -> 72965184
+KNLwr5bK
+4j8johPs9L
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+
+const movies = [
+    "Alladyn",
+    "Sinbad",
+    "John Wick",
+    "Lucyfer",
+    "Sabrina",
+    "Game of Thrones",
+    "Klan"
+]
+
+const app = express()
+app.use(express.json())
+
+app.get('/api/movies', (req, res) => {
+    res.send(movies)
+})
+
+app.get('/api/movies/:id', (req, res) => {
+    const movieId = req.params.id
+    if (movies[movieId]) {
+        res.send(movies[movieId])
+    } else {
+        res.status(401).send("Bad request")
+    }
+})
+
+app.post('/api/movies', (req, res) => {
+    const { movie } = req.body 
+    
+    if ( !movies.includes(movie) ) {
+        movies.push(movie)
+        res.send(movies)
+    } else {
+        res.status(400).send("Movies already in our list")
+    }
+})
+
+app.put('/api/movies/:id', (req, res) => {
+    //Napisac updaty listy
+})
+
+//{ "movie": "Spiderman" }
+//npm i --global nodemon
+//localhost:3000/api/movies
+app.listen(3000, ()=> console.log('Running...'))

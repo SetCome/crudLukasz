3c4332a9e5a42aec6fcf96e4f117982a587896ed
Piotr <no@reply.com>
2020-03-05 19:40:41 +0100

    abc

--- index.js
+++ index.js
@@ -38,10 +38,35 @@ app.post('/api/movies', (req, res) => {
 })
 
 app.put('/api/movies/:id', (req, res) => {
+    const movieId = req.params.id
+    if(!movies[movieId]){
+        res.status(401).send("Bad request")
+        return;
+    }
+    const { movie } = req.body
+    if (movies.includes(movie)){
+        res.status(400).send("Movies already in our list");
+        return;
+    }
+    movies[movieId] = movie;
+    res.send(movies)
     //Napisac updaty listy
 })
 
-//{ "movie": "Spiderman" }
-//npm i --global nodemon
-//localhost:3000/api/movies
+function usuńElement(tab,n) {
+    let początek = tab.slice(0, n)
+    let koniec = tab.slice(n+1)
+    return [...początek, ...koniec];
+}
+
+app.delete('/api/movies/:id', (req, res) => {
+    const id = req.params.id;
+    if(!movies[id]) {
+        res.status(401).send("Bad request - incorrect movie id")
+        return;
+    }
+    movies = usuńElement(movies, id);
+    res.send(movies)
+    });
+
 app.listen(3000, ()=> console.log('Running...'))
